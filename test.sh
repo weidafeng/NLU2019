fbdf342eb081b5567c6561d224bff370184ba423
wdf <weidafeng.edu@gmail.com>
2019-06-09 18:31:34 +0800

    update run scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-python run_classifier.py \
+export CUDA_VISIBLE_DEVICES=0
+
+python ./model/run_classifier.py \
 	--task_name QNLI \
-	--do_test \
+	--do_predict \
 	--do_lower_case \
 	--data_dir ./glue_data/QNLI \
 	--bert_model bert-base-uncased \
--- train.sh
+++ train.sh
@@ -1,13 +1,15 @@
-python run_classifier.py \
+export CUDA_VISIBLE_DEVICES=0
+
+python ./model/run_classifier.py \
 	--task_name QNLI \
 	--do_train \
 	--do_eval \
 	--do_lower_case \
-	--cache_dir ./bert-base-uncased
+	--cache_dir ./bert-base-uncased \
 	--data_dir ./glue_data/QNLI \
 	--bert_model bert-base-uncased \
 	--max_seq_length 128 \
 	--train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--num_train_epochs 50.0 \
+	--num_train_epochs 20.0 \
 	--output_dir ./glue_data/QNLI/results
